60451ba1a7a5b149002cfbf46b7ac7c3c684c63b
Abd Elrahman Atef <65117957+Boody2004@users.noreply.github.com>
2026-02-21 16:30:36 +0200

    Type Annotations

--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -1,16 +1,31 @@
 void main() {
-  var name = "Abdelrahman Atef";
+  // var name = "Abdelrahman Atef";
+  // print(name);
+
+  // final age = 21;
+  // print(age);
+
+  // print(age - 10);
+  // print(age * 10);
+  // print(age + 10);
+
+  // const isOpen = true;
+  // print(isOpen);
+
+  // print("My name is $name an my age is $age");
+
+  const String name = "Boody";
   print(name);
 
-  final age = 21;
+  int age = 21;
+  age = 25;
   print(age);
 
-  print(age - 10);
-  print(age * 10);
-  print(age + 10);
-
-  const isOpen = true;
+  bool isOpen = true;
+  isOpen = false;
   print(isOpen);
 
-  print("My name is $name an my age is $age");
+  double averageRating = 7;
+  averageRating = 7.9;
+  print(averageRating);
 }
